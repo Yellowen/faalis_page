bde796bd01c7a6f17ba6b390782f5797e64ea268
Sameer Rahmani <lxsameer@gnu.org>
2014-05-29 16:17:24 +0430

    Some improvements

--- lib/faalis_page.rb
+++ lib/faalis_page.rb
@@ -1,6 +1,7 @@
-require "faalis_page/engine"
-
+# This module contains related classes and modules to
+# work with `Faalis` pages
 module FaalisPage
 end
 
 require 'faalis_page/engine'
+require 'faalis_page/version'
--- lib/faalis_page/engine.rb
+++ lib/faalis_page/engine.rb
@@ -2,19 +2,20 @@ if File.exists?([File.expand_path("../../../", __FILE__),
                  ".development"].join("/"))
   $LOAD_PATH <<  File.expand_path('../../../../Faalis/lib', __FILE__)
 end
-
 require "faalis"
 
 
 module FaalisPage
+  # Page engine class for Faalis page plugin
+  # mount this engine under `/`
   class Engine < ::Rails::Engine
-  engine_name "faalis_page"
+  engine_name 'faalis_page'
 
     ::Faalis::Engine.setup do |config|
       config.models_with_permission = []
     end
 
-    ::Faalis::Plugins.register "faalis_page", self
-    ::Faalis::Engine.dashboard_js_manifest = "faalis_page/application.js"
+    ::Faalis::Plugins.register 'faalis_page', self
+    ::Faalis::Engine.dashboard_js_manifest = 'faalis_page/application.js'
   end
 end
