a79fb526c77508f28a33b0ccf269be5d0d619754
Sameer Rahmani <lxsameer@gnu.org>
2016-04-06 12:23:21 +0430

    Old feature dir removed and minitest turned on as defualt
    Why:
    
    We need minitest as test suite

--- lib/faalis/page/engine.rb
+++ lib/faalis/page/engine.rb
@@ -5,6 +5,14 @@ module Faalis::Page
     engine_name 'faalis_page'
     isolate_namespace Faalis::Page
 
+    config.generators do |g|
+      g.test_framework      :minitest, fixture_replacement: :fabrication
+      g.fixture_replacement :fabrication, dir: "test/fabricators"
+      g.integration_tool    :minitest
+      g.assets              false
+      g.helper              false
+    end
+
     mattr_accessor :use_site_framework
     @@use_site_framework = false
 
