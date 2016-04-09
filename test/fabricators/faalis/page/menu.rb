a8937601670507ae8d451c62ea45f7293f00ad14
Sameer Rahmani <lxsameer@gnu.org>
2016-04-09 14:20:01 +0430

    Menu fabricator and tests added.

---
+++ test/fabricators/faalis/page/menu.rb
@@ -0,0 +1,7 @@
+Fabricator :menu, class_name: 'Faalis::Page::Menu' do
+  title 'example menu'
+  site_id nil
+  published true
+  domain_id nil
+  items ""
+end
--- test/models/faalis/menu_test.rb
+++ test/models/faalis/menu_test.rb
@@ -2,8 +2,33 @@ require 'test_helper'
 
 module Faalis::Page
   class MenuTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    @@subject = ::Faalis::Page::Menu
+
+    test "saves with a title" do
+      subject = Fabricate(:menu, title: 'example')
+
+      assert subject.save, "Can't save page due to: #{subject.errors.to_json}"
+    end
+
+    test "won't save without a title" do
+      subject = @@subject.new(title: nil)
+
+      assert_not subject.save, "Page saved. It shouldn't without a title"
+      assert_equal subject.errors.keys[0], :title
+    end
+
+    test "`published` scope should returns only published pages" do
+      1.upto(4) do
+        Fabricate(:menu, title: "example", published: true)
+      end
+      Fabricate(:menu, title: "example")
+      msg = "It seems that `published` scope returns all objects"
+
+      result = @@subject.published
+
+      assert_equal result.count, 4, msg: msg
+    end
+
   end
 end
